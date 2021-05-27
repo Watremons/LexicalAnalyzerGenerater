0c4fb03c21cf445733adb46777bc66599d2f7c27
Watremons <z2331355763@sina.com>
2021-05-27 11:00:59 +0800

    🧪 test(project): 增加测试用例
    增加test.py用于测试

--- project/test.py
+++ project/test.py
@@ -1,29 +1,59 @@
-from project.lib.element import Element, ElementType
+from project.lib.dfa import DfaGraph, DfaState, DfaEdge
+from project.syntaxAnalysis import doSyntaxAnalysis
+from project.lib.regularExpression import RegularExpression
+
+import os
+
+
+# Function: get path of analysisTable.txt
+def getInputFilePath(filename):
+    nowPath = os.path.dirname(__file__)
+
+    dirPath = nowPath
+    if (os.path.isdir(dirPath)):
+        inputFilePath = os.path.join(dirPath, filename)
+
+        if (os.path.isfile(inputFilePath)):
+            return inputFilePath
+        else:
+            print("The file {} is not exist!".format(inputFilePath))
+            return None
+    else:
+        print("The dir {} is not exist!".format(nowPath))
+        return None
+
+
+# Function: do lexcial analysis
+def parseTextToLexeme(dfaGraphList: list, inputText: str) -> list:
+    pass
+
+
+# Function: do test by real code
+def testDfaGraphByFile(dfaGraphList: list, inputFileName: str) -> list:
+    filePath = getInputFilePath(inputFileName)
+    inputText = ""
+    if filePath is not None:
+        f = open(filePath, 'r', encoding='utf-8')
+        inputText = f.read()
+        f.close()
+    else:
+        print("Can't get input file!")
+        return
+
+    resultList = parseTextToLexeme(dfaGraphList, inputText)
+    return resultList
 
 
 if __name__ == "__main__":
-    newSet = set()
-    element = Element(
-        classType=ElementType.CHARACTOR,
-        name="1"
-    )
-    newSet.add(element)
-    newSet.add(Element(
-        classType=ElementType.CHARACTOR,
-        name="1"
-    ))
-    newSet.add(Element(
-        classType=ElementType.CHARACTOR,
-        name="2"
-    ))
-    newSet.add(1)
-    newSet.add(111)
+    reList = doLexicalAnalysis()
 
-    num = 0
-    newSetCopy = newSet.copy()
-    for element in newSetCopy:
-        print(element)
-        newSet.add(num)
+    dfaGraphList = doSyntaxAnalysis(reList)
 
-    sameValues = newSet & newSetCopy
-    print(len(sameValues))
+    # # Unfinished: do test and get return
+    # inputFileName = input("Lexer has already generated.\nPlease input the code filename:")
+    inputFileName = "sample.tny"
+    lexemeList = testDfaGraphByFile(dfaGraphList=dfaGraphList, inputFilename=inputFileName)
+    for lexeme in lexemeList:
+        print(lexeme)
+    x = RegularExpression.LexcialType["NONE"]
+    print(x)
