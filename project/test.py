c164c8a476cfdc9a6581c64f97ba745d54471f54
Watremons <z2331355763@sina.com>
2021-05-30 14:52:17 +0800

    ✨ feat(test): 增加报错信息提示；完成测试函数；
    增加语法分析报错提示；完成测试函数输入与输出

--- project/test.py
+++ project/test.py
@@ -1,6 +1,7 @@
 from project.lib.dfa import DfaGraph, DfaState, DfaEdge
 from project.syntaxAnalysis import doSyntaxAnalysis
 from project.lib.regularExpression import RegularExpression
+from project.lexicalAnalysis import doLexicalAnalysis
 
 import os
 
@@ -24,12 +25,76 @@ def getInputFilePath(filename):
 
 
 # Function: do lexcial analysis
-def parseTextToLexeme(dfaGraphList: list, inputText: str) -> list:
-    pass
+def parseTextToLexeme(dfaGraph: DfaGraph, inputText: str) -> list:
+    returnList = []
+    typeDict = {v: k for k, v in RegularExpression.LexcialType.items()}
+    print(typeDict)
+    lexemeText = inputText.replace("\n", " ").replace("\t", " ")
+    print(lexemeText)
+    nowState = dfaGraph.startState
+    lexeme = ""
+    index = 0
+
+    # Get one char a time and traverse the dfa
+    while index < len(lexemeText):
+        nowChar = lexemeText[index]
+        hasNext = False
+        anyDriverCharState = None
+
+        # For a char, search for a edge and move to next state
+        for edge in nowState.edgeList:
+            # Set a anyDriverCharState to be picked if there is not a match edge
+            if edge.driverChar == "^":
+                anyDriverCharState = edge.nextState
+            if edge.driverChar == nowChar:
+                nowState = edge.nextState
+                lexeme = lexeme + nowChar
+                hasNext = True
+                break
+
+        # Can't move, check whether nowState is an endState
+        # Or nowState has an anyDriverCharState to move
+        if not hasNext:
+            # if nowState has an anyDriverCharState to move
+            if anyDriverCharState is not None:
+                nowState = anyDriverCharState
+                lexeme = lexeme + nowChar
+            else:
+                # Check whether nowState is the start state
+                if nowState.stateId != dfaGraph.startState.stateId:
+                    # Check whether nowState is an endState
+                    if nowState.isEndState:
+                        returnList.append({lexeme: typeDict[nowState.endStateClass]})
+                    else:
+                        returnList.append({lexeme: typeDict[0]})
+                    nowState = dfaGraph.startState
+                    lexeme = ""
+                    index = index - 1
+                # if so, clear lexeme
+                else:
+                    lexeme = ""
+        # Get next char
+        index = index + 1
+
+    # Check whether nowState is the start state
+    if nowState.stateId != dfaGraph.startState.stateId:
+        # Check whether nowState is an endState
+        if nowState.isEndState:
+            returnList.append({lexeme: typeDict[nowState.endStateClass]})
+        else:
+            returnList.append({lexeme: typeDict[0]})
+        nowState = dfaGraph.startState
+        lexeme = ""
+
+    # if so, clear lexeme
+    else:
+        lexeme = ""
+
+    return returnList
 
 
 # Function: do test by real code
-def testDfaGraphByFile(dfaGraphList: list, inputFileName: str) -> list:
+def testDfaGraphByFile(dfaGraph: DfaGraph, inputFileName: str) -> list:
     filePath = getInputFilePath(inputFileName)
     inputText = ""
     if filePath is not None:
@@ -40,20 +105,22 @@ def testDfaGraphByFile(dfaGraphList: list, inputFileName: str) -> list:
         print("Can't get input file!")
         return
 
-    resultList = parseTextToLexeme(dfaGraphList, inputText)
+    resultList = parseTextToLexeme(dfaGraph, inputText)
     return resultList
 
 
 if __name__ == "__main__":
     reList = doLexicalAnalysis()
 
-    dfaGraphList = doSyntaxAnalysis(reList)
-
-    # # Unfinished: do test and get return
+    dfaGraph = doSyntaxAnalysis(reList)
+    if dfaGraph == "error":
+        print("Error: the input regular expressions have syntax error")
+        exit(1)
+    print("\n")
+    # Finished: do test and get return
     # inputFileName = input("Lexer has already generated.\nPlease input the code filename:")
     inputFileName = "sample.tny"
-    lexemeList = testDfaGraphByFile(dfaGraphList=dfaGraphList, inputFilename=inputFileName)
+    lexemeList = testDfaGraphByFile(dfaGraph=dfaGraph, inputFileName=inputFileName)
     for lexeme in lexemeList:
         print(lexeme)
-    x = RegularExpression.LexcialType["NONE"]
-    print(x)
+    exit(0)
